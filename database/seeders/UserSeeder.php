2d5dfa3757714c38b2901d4e0b7d4bbb8aa1ff01
iAmM_777 <m02403992@gmail.com>
2026-02-12 12:27:58 +0100

    Laravel opdracht 13: User Seeder
      ✓ UserSeeder exists                                                                                                                                   0.26s
      ✓ correct users are created                                                                                                                           0.04s
      ✓ users have correct roles                                                                                                                            0.04s
    
       PASS  Tests\Feature\Opdrachten\Opdracht13Test
      ✓ the student user can login with email and correct password                                                                                          0.13s
      ✓ the teacher user can login with email and correct password                                                                                          0.08s
      ✓ the admin user can login with email and correct password

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // student
+        $student = User::create([
+            'name' => 'student',
+            'email' => 'student@school.nl',
+            'password' => Hash::make('student'),
+        ]);
+        $student->assignRole('student');
+
+        // teacher
+        $teacher = User::create([
+            'name' => 'teacher',
+            'email' => 'teacher@school.nl',
+            'password' => Hash::make('teacher'),
+        ]);
+        $teacher->assignRole('teacher');
+
+        // admin
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@school.nl',
+            'password' => Hash::make('admin'),
+        ]);
+        $admin->assignRole('admin');
+    }
+}
