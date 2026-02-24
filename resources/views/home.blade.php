43eff848c3c1316592f4fa78aaccd339c3b76dd5
iAmM_777 <149480656+iAmM777@users.noreply.github.com>
2026-02-24 23:28:53 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
             <p class="text-lg text-gray-600 mb-8">Hallo, {{ auth()->user()->name }}!</p>
             <a href="{{ route('logout') }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Uitloggen</a>
         @else
-            <p class="text-lg text-gray-600 mb-8">Gelieve in te loggen of te registreren om toegang te krijgen tot de applicatie.</p>
+            <p class="text-lg text-gray-600 mb-8">Login of registreer om toegang te krijgen tot de applicatie.</p>
             <div class="mt-8">
                 <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">Inloggen</a>
                 <a href="{{ route('register') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Registreren</a>
