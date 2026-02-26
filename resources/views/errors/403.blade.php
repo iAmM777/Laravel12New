caf47086c33b5b1c8875e2c2d69260b07113a8e1
iAmM_777 <m02403992@gmail.com>
2026-02-26 13:35:29 +0100

    .\vendor\bin\pest --group=Opdracht14 fix
    just a comment

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,7 +4,8 @@
 <div class="container mx-auto max-w-4xl py-12">
     <div class="text-center">
         <h1 class="text-4xl font-bold text-gray-800 mb-4">403 - Toegang Geweigerd</h1>
-        <p class="text-lg text-gray-600 mb-8">User heeft geen authorizatie om deze pagina te bekijken.</p>
+        <!--<p class="text-lg text-gray-600 mb-8">User is not logged in.</p>-->
+        <p class="text-gray-500">Je hebt geen toestemming om deze pagina te bekijken.</p>
         <p class="text-gray-500">Neem contact op met de beheerder.</p>
         <!--
         <div class="mt-8">
