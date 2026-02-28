9a25f493af0f1523f1a05f1eeddfa13560e5f936
iAmM_777 <149480656+iAmM777@users.noreply.github.com>
2026-02-28 22:47:03 +0100

    Revert "reverting"
    This reverts commit 80182e1758810c30ea4a9bb87af3eba15c52d86b.

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // create a bunch of tasks; factories will handle related models if necessary
+        Task::factory()->create();
+    }
+}
